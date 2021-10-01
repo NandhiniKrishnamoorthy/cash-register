cc22d4a1ca450cbbe5cb9058965f4c357b6d8dee
NandhiniKrishnamoorthy <nandhiniprasath25@gmail.com>
2021-09-30 22:11:34 -0700

    Docs: Updated js

--- index.js
+++ index.js
@@ -10,24 +10,30 @@ const notes = [2000, 500, 100, 20, 10, 5, 1];
 
 calculateButton.addEventListener("click", function calculateAmount() {
     errorMessage.style.display = "none";
-    if (totalBill.value > 0) {
-        debugger;
-        let billAmount = Number(totalBill.value);
-        let cashGiven = Number(totalCash.value);
-        if (cashGiven > billAmount) {            
-            const remainingAmount = cashGiven - billAmount;
-            calculateChangeToReturn(remainingAmount);            
-        }
-        else if (cashGiven === billAmount) {
-            displayMessage("No Cash to return");
+    if (totalBill.value && totalCash.value) {
+        if (totalBill.value > 0 && totalCash.value) {
+            debugger;
+            let billAmount = Number(totalBill.value);
+            let cashGiven = Number(totalCash.value);
+            if (cashGiven > billAmount) {
+                const remainingAmount = cashGiven - billAmount;
+                calculateChangeToReturn(remainingAmount);
+            }
+            else if (cashGiven === billAmount) {
+                displayMessage("No Cash to return");
+            }
+            else {
+                displayMessage("Cash amount should be greater than bill amount");
+            }
         }
         else {
-            displayMessage("Cash amount should be greater than bill amount");
+            displayMessage("Please enter positive amount");
         }
     }
     else {
-        displayMessage("Please enter positive bill amount");
+        displayMessage("Please enter the values");
     }
+    
 });
 
 function calculateChangeToReturn(cash) {    
